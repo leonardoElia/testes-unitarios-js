9d0bdb2bf6d79ca3fc11648c49e2f86a78a4cc17
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2022-08-18 16:47:57 -0300

    Fazendo 9 requesitos

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,37 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toEqual(undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+      let Arya = getCharacter('Arya');
+      expect(Arya.class).toEqual('Rogue')
+      expect(Arya.name).toEqual('Arya Stark')
+      expect(Arya.phrases[0]).toEqual('Not today')
+      expect(Arya.phrases[1]).toEqual('A girl has no name.')
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    let Brienne = getCharacter('Brienne');
+    expect(Brienne.class).toEqual('Knight')
+    expect(Brienne.name).toEqual('Brienne Tarth')
+    expect(Brienne.phrases[0]).toEqual('Im No Lady, Your Grace.')
+    expect(Brienne.phrases[1]).toEqual('I, Brienne Of Tarth, Sentence You To Die.')
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    let Melissandre = getCharacter('Melissandre');
+    expect(Melissandre.class).toEqual('Necromancer')
+    expect(Melissandre.name).toEqual('Melissandre')
+    expect(Melissandre.phrases[0]).toEqual('Death By Fire Is The Purest Death.')
+    expect(Melissandre.phrases[1]).toEqual('For The Night Is Dark And Full Of Terrors.')
+
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    let testeCase = getCharacter('MeLissAndre');
+    expect(testeCase.class).toEqual('Necromancer')
+    expect(testeCase.name).toEqual('Melissandre')
+    expect(testeCase.phrases[0]).toEqual('Death By Fire Is The Purest Death.')
+    expect(testeCase.phrases[1]).toEqual('For The Night Is Dark And Full Of Terrors.')
+
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('leonardo')).toEqual(undefined);
   });
 });
